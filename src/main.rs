9c588a8e954398189f6a1524498c92bb6b5ca3ab
euamcg <68219791+euamcg@users.noreply.github.com>
2023-08-14 15:52:03 +0100

    Updated to pull from git
    	modified:   Cargo.toml
    	modified:   src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 use std::time::Duration;
 
 use tracing::{
+    debug,
     dispatcher::{self, Dispatch},
-    error, info,
+    error, info, trace, warn,
 };
 use tracing_logfmt;
 use tracing_subscriber::{layer::SubscriberExt, Registry};
@@ -11,9 +12,14 @@ fn main() {
     let sub = Registry::default().with(tracing_logfmt::layer());
     dispatcher::set_global_default(Dispatch::new(sub)).expect("Global logger already set");
     loop {
-        info!("Starting something");
+        info!("Does this work?");
         std::thread::sleep(Duration::from_millis(3000));
         error!("Does this work?");
-        std::thread::sleep(Duration::from_millis(2000))
+        std::thread::sleep(Duration::from_millis(3000));
+        trace!("Does this work?");
+        std::thread::sleep(Duration::from_millis(3000));
+        debug!("Does this work?");
+        std::thread::sleep(Duration::from_millis(3000));
+        warn!("Does this work?");
     }
 }
